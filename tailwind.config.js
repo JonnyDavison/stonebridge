db7dd27a1417c8fb1fa785420661a97fe6af99fc
JonnyDavison <jonnydavison@icloud.com>
2025-01-02 21:05:58 +0000

    Update CSS, run build

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,24 @@ module.exports = {
     './static/**/*.js',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       spacing: {
         '96': '24rem',
       },
       colors: {
-        'custom-color1': '#6c705f',
-        'custom-color2': '#eee3d4',
-        'custom-color3': '#b8b7a5',
-        'custom-color4': '#000000',
+        'custom-color1': '#6C733D',
+        'custom-color2': '#F2F2F2',
+        'custom-color3': '#202426',
+        'custom-color4': '#8C8C88',
+        'custom-color5': '#9DA65D',
+
       },
       fontFamily: {
         'body': ['Syne', 'sans-serif'],
